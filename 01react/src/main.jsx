343c695e1897a37dcbdbc5af553742d7dc0d5a50
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-07-05 14:11:39 +0000

    more on react

--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -3,15 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 function MyApp(){
+  const username ="chai aur code"
   return(
     <div>
-      <h1>Custom App</h1>
+      <h1>Custom App{username} ||</h1>
     </div>
   )
 }
 
+// const ReactElement = {
+//   type:'a',
+//   props:{
+//       herf:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// } 
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit Google</a>
+)
+
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',targer:'_blank'},
+  'click me to visit google'
+
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MyApp/>
-  </React.StrictMode>,
+  <App/>
 )
